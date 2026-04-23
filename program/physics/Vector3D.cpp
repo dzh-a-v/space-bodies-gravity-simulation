385d6de05ec893b868905cbd9900de070819ed27
dzh-a-v <metroexoduslover@gmail.com>
2026-04-23 14:55:15 +0300

    change: switch to python

--- program/physics/Vector3D.cpp
+++
@@ -1,7 +0,0 @@
-#include "Vector3D.h"
-
-namespace physics {
-
-// Vector3D implementation is entirely in header (inline methods)
-
-} // namespace physics
--- program/physics/VerletIntegrator.cpp
+++
@@ -1,7 +0,0 @@
-#include "VerletIntegrator.h"
-
-namespace physics {
-
-// VerletIntegrator implementation is entirely in header (template class)
-
-} // namespace physics
